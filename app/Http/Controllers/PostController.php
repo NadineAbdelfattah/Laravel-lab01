da6241f905c2e64139836bfc0368f706f5bc8398
NadineAbdelfattah <78588221+NadineAbdelfattah@users.noreply.github.com>
2021-03-19 02:43:20 +0200

    Select from DB

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts=[
-            ['id'=>1, 'title'=>'Laravel', 'description'=>'This is description','posted_by'=>'Nadine','created_at'=>'2021-03-13'],
-            ['id'=>2, 'title'=>'Javascript','description'=>'This is description', 'posted_by'=>'Nour','created_at'=>'2021-03-13']
-        ];
+        // $posts=[
+        //     ['id'=>1, 'title'=>'Laravel', 'description'=>'This is description','posted_by'=>'Nadine','created_at'=>'2021-03-13'],
+        //     ['id'=>2, 'title'=>'Javascript','description'=>'This is description', 'posted_by'=>'Nour','created_at'=>'2021-03-13']
+        // ];
+        $posts = Post::all();//select*from posts;
         return view('posts.index', [
             'posts'=>$posts
         ]);
